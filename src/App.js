4c01fd0161d81c26a1fbbe077fa9d790aca4fed4
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-11-06 00:12:48 -0400

    Finished setting up Craco/TailwindConfig

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          <h1>Welcome</h1>
+          <h1>Hellooooo</h1>
         </p>
         <a
           className="App-link"
