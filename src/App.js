e689d55fe72f41b0a41a322d92b50e32932d4fdc
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-11-05 22:38:24 -0400

    CHanged title again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>HEELLLLOOOOO</h1>
+          <h1>Welcome</h1>
         </p>
         <a
           className="App-link"
